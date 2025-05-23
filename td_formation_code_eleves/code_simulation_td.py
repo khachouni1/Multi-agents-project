cec39cce921cd90e4ac52a585bd7d6289eb4c550
Mohamed IDRISSI <simoidrissi2003@gmail.com>
2025-05-23 16:58:24 +0200

    modif

--- td_formation_code_eleves/code_simulation_td.py
+++ td_formation_code_eleves/code_simulation_td.py
@@ -1,116 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-@author: Sylvain Bertrand, 2023
-
-
-Simulation script of a Multi-Agent System
-
-Agent dynamics can either be:
-    -singleIntegrator2D:    x_dot = ux,   y_dot = uy
-                            state : [x, y]
-                            control input: [vx, vy]
-    -unicycle:   x_dot = V.cos(theta),   y_dot = V.sin(theta),   theta_dot = omega
-                            state : [x, y, theta]
-                            control input: [V, omega] 
-                            
-    A conversion function is provided from single integrator inputs to unicycle inputs 
-        [V, omega] = si_to_uni( [vx, vy], theta, kp=angular_speed_proportional_gain )
-
+Simulation : 2 camions + 4 drones avec répulsion
 """
 
 import numpy as np
 from lib.simulation import FleetSimulation
 from lib.robot import Fleet, si_to_uni
 import control_algo
-from lib.mission import plot_mission_background 
 import matplotlib.pyplot as plt
 
+# === Paramètres ===
+nbOfRobots = 6
+robotDynamics = 'singleIntegrator2D'
 
+initPositions = np.array([
+    [-6, -6],     # Camion 1
+    [6, 6],       # Camion 2
+    [-10, -10],   # Drone 1
+    [-10, -6],    # Drone 2
+    [10, -10],    # Drone 3
+    [10, -6]      # Drone 4
+])
 
-# number of robots
-# -----------------
-nbOfRobots = 2
-
-# dynamics of robots
-# -------------------
-robotDynamics = 'singleIntegrator2D'    # use 'signleIntegrator2D' or 'unicycle'
-
-
-# initial states of robots 
-# --------------------------
-
-# ... initial positions randomly defined 
-#initPositions = 20*np.random.rand(nbOfRobots,2)-10  # random init btw -10, +10
-
-# ... initial positions defined from data    (dimension: nb of agents  x  2)
-initPositions = np.array([[- 6, - 10],       # x-coordinates (m)
-                          [- 6, - 10]]).T   # y-coordinates (m)
-
-
-# ... initial orientation angles and poses (USED FOR UNICYCLE DYNAMICS ONLY)
-if (robotDynamics=='unicycle'):
-    # orientation angles (rad)    (dimension: nb of agnts x 1)
-    initAngles = np.array([[0., 0., 0., 0.]]).T
-    initPoses = np.concatenate((initPositions, initAngles), axis=1)
-
-
-
-# create fleet
-if (robotDynamics=='singleIntegrator2D'):
+if robotDynamics == 'singleIntegrator2D':
     fleet = Fleet(nbOfRobots, dynamics=robotDynamics, initStates=initPositions)
 else:
+    initAngles = np.zeros((nbOfRobots, 1))
+    initPoses = np.concatenate((initPositions, initAngles), axis=1)
     fleet = Fleet(nbOfRobots, dynamics=robotDynamics, initStates=initPoses)
 
-
-# sampling period for simulation
 Ts = 0.01
-
-# create simulation
 simulation = FleetSimulation(fleet, t0=0.0, tf=400.0, dt=Ts)
 
-   
-# simulation loop
 for t in simulation.t:
-
-    # get poses of robots as a single array of size (nb_of_robots, nb_of_states)
-    robots_poses = fleet.getPosesArray()    
-
-    # compute control input of each robot
+    robots_poses = fleet.getPosesArray()
     for robotNo in range(fleet.nbOfRobots):
-        
-        vx, vy = control_algo.formation(t, robotNo, robots_poses)     # <= MODIFY CONTROL LAW IN "control_algo.py"
-        
-        if (robotDynamics=='singleIntegrator2D'):
-            fleet.robot[robotNo].ctrl = np.array([vx, vy]) 
-        else:
-            fleet.robot[robotNo].ctrl = si_to_uni(vx, vy, robots_poses[robotNo,2], kp=10.) 
-    
-    
-    # update data of simulation 
+        vx, vy = control_algo.formation(t, robotNo, robots_poses)
+        fleet.robot[robotNo].ctrl = np.array([vx, vy])
     simulation.addDataFromFleet(fleet)
-
-    # integrate motion of the fleet
     fleet.integrateMotion(Ts)
 
-
-# plot animation
-#simulation.animation(figNo=1, pause=0.00001, robot_scale=1.0)   
-
-# plot 2D trajectories
+# === Visualisation simple (compatibilité assurée)
 simulation.plotXY(figNo=2)
-# plot on current figure the mission background for Question 1.6
-#plot_mission_background()
-
-
-# plot states' components Vs time
 simulation.plotState(figNo=3)
+simulation.plotCtrl(figNo=4)
 
-# plot control inputs' components Vs time
-simulation.plotCtrl(figNo=6)
-
-# plot 2D trajectories (every 'X steps' time instants
-#simulation.plotXY(figNo=10, steps=50, links=True)
-# plot on current figure the mission background for Question 1.6
-#plot_mission_background()
-
-plt.show(block=True)
+plt.show()
--- td_formation_code_eleves/control_algo.py
+++ td_formation_code_eleves/control_algo.py
@@ -1,170 +1,90 @@
 # -*- coding: utf-8 -*-
 """
-author: Sylvain Bertrand, 2023
-
-   All variables are in SI units
-    
-   
-   Variables used by the functions of this script
-    - t: time instant (s)
-    - robotNo: no of the current robot for which control is coputed (0 .. nbRobots-1)
-    - poses:  size (3 x nbRobots)
-        eg. of use: the pose of robot 'robotNo' can be obtained by: poses[:,robotNo]
-            poses[robotNo,0]: x-coordinate of robot position (in m)
-            poses[robotNo,1]: y-coordinate of robot position (in m)
-            poses[robotNo,2]: orientation angle of robot (in rad)   (in case of unicycle dynamics only)
+Contrôle multi-agent : 2 camions, 4 drones
+- Chaque drone dessert un camion
+- Seul le plus proche de chaque camion agit
+- Répulsion entre drones activée
 """
-
 
 import numpy as np
 import math
 
+# === Fonction de répulsion entre drones ===
+def repulsion(pos_self, pos_other, threshold=3.0, gain=10.0):
+    direction = pos_self - pos_other
+    distance = np.linalg.norm(direction)
+    if distance < threshold and distance > 1e-6:
+        force = gain * (1.0 / distance - 1.0 / threshold) * (direction / distance)
+        return force
+    else:
+        return np.zeros_like(pos_self)
 
-
-# ==============   "GLOBAL" VARIABLES KNOWN BY ALL THE FUNCTIONS ==============
-# all variables declared here will be known by functions below
-# use keyword "global" inside a function if the variable needs to be modified by the function
-
-
-# global toto
-
-global firstCall   # this variable can be used to check the first call ever of a function
+# === Variables globales ===
 firstCall = True
-
-global a_visiter   # Variable qui indice les points à visiter par le camion
-a_visiter = 0
-
-global Plein       # Si le drone est plein, il rejoint le camion, s'il est vide, il rejoint le point de ravitaillement
-Plein = True
-
-global nbr_aller_retour # Compte le nombre d'aller retour que réalise le drone
-nbr_aller_retour = 0
+a_visiter = [0, 0]  # index de point à visiter par chaque camion
+Plein = [True] * 4
+nbr_aller_retour = [0] * 4
 
 
-# =============================================================================
-
-
-
-
-# =============================================================================
 def formation(t, robotNo, robots_poses):
-# =============================================================================  
+    global firstCall, a_visiter, Plein, nbr_aller_retour
 
-    # --- example of modification of global variables ---
-    # ---(updated values of global variables will be known at next call of this funtion) ---
-    # global toto
-    # toto = toto +1
-    global firstCall
-    global a_visiter
-    global Plein
-    global nbr_aller_retour
-
-    
-    
-    
-    # number of robots (short notation)
     N = robots_poses.shape[0]
-    
-    # get index of current robot  (short notation)
     i = robotNo
-   
-    # get positions of all robots
-    x = robots_poses[:,0:2]
-     
-    # control law
-    vx = 0.
-    vy = 0.
-
-
-    # adjacencdy matrix of communication graph
-    # -----------------------------------------
-    A= np.ones((N, N)) - np.eye(N)
-
-    A[0, 1] = 0
-    
-    if (firstCall):  # print information (only once)
-        print(A)
-        
-        firstCall = False
-    
-
-    
-    
-    # initialize control input vector
+    x = robots_poses[:, :2]
     ui = np.zeros(2)
+
     kp = 0.1
     kp2 = 0.2
-    
-    # Points à visiter pour le camion
 
-    visit = np.array([[- 6, 6], [6, 6], [6, - 6], [- 6, - 6]])  # Points que le camion doit visiter
-    origin_drone = [- 10, - 10]                                 # Points de ravitaillement du drone
+    visits = [
+        np.array([[-6, 6], [6, 6], [6, -6], [-6, -6]]),
+        np.array([[4, 4], [4, -4], [-4, -4], [-4, 4]])
+    ]
+    origin_drones = np.array([[-10, -10], [-10, -6], [10, -10], [10, -6]])
 
+    if firstCall:
+        print("=== Contrôle multi-agent : 2 camions, 4 drones + répulsion ===")
+        firstCall = False
 
+    # Camions
+    if i in [0, 1]:
+        if np.linalg.norm(x[i] - visits[i][a_visiter[i]]) < 0.2:
+            a_visiter[i] = (a_visiter[i] + 1) % 4
+        ui = -kp * (x[i] - visits[i][a_visiter[i]])
 
-    # ===================== COMPUTATION OF ui =================================
-        
+    # Drones
+    elif i >= 2:
+        drone_id = i - 2
+        if nbr_aller_retour[drone_id] >= 3:
+            return 0.0, 0.0
 
-    # Camion
-    if i == 0:
-        if math.dist(x[i], visit[a_visiter]) < 0.05:    # Si la distance entre le point fcitf et le camion assez faible, on change de point
-            a_visiter = (a_visiter + 1) % 4
-        ui = ui - kp*(x[i] - visit[a_visiter])
-    
-    # Drone
-    else:
-        if nbr_aller_retour < 3:
-            if Plein:
-                if math.dist(x[i], x[0]) < 0.1:         # Si la distance entre le drone et le camion est assez faible, il se vide
-                    Plein = False
+        camion_id = drone_id % 2
+        drone_indices = [j for j in range(4) if j % 2 == camion_id]
+        global_indices = [j + 2 for j in drone_indices]
+        distances = [np.linalg.norm(x[j] - x[camion_id]) for j in global_indices]
+        closest = global_indices[np.argmin(distances)]
+        closest_base = origin_drones[drone_id]
+
+        if i == closest:
+            if Plein[drone_id]:
+                if np.linalg.norm(x[i] - x[camion_id]) < 0.2:
+                    Plein[drone_id] = False
             else:
-                if math.dist(x[i], origin_drone) < 0.1: # Si la distance entre le drone et le point de ravitaillement est assez faible, il se remplit
-                    Plein = True
-                    nbr_aller_retour += 1    
-            if Plein:
-                ui = ui - kp2*(x[i] - x[0])             # Si le drone est plein, il rejoint le camion
+                if np.linalg.norm(x[i] - closest_base) < 0.2:
+                    Plein[drone_id] = True
+                    nbr_aller_retour[drone_id] += 1
+
+            if Plein[drone_id]:
+                ui = -kp2 * (x[i] - x[camion_id])
             else:
-                ui = ui - kp2*(x[i] - origin_drone)     # Si le drone est vide, il rejoint le point de ravitaillement
+                ui = -kp2 * (x[i] - closest_base)
+        else:
+            ui = -kp2 * (x[i] - closest_base)
 
-        
-    
-    # =========================================================================
-    
-    
-    # retrieve values from control input vector
-    vx = ui[0]
-    vy = ui[1]
-    
-    
-    return vx, vy
-# =============================================================================
+        # Ajout répulsion avec autres drones
+        for j in range(2, N):
+            if j != i:
+                ui += repulsion(x[i], x[j])
 
-
-
-
-
-
-
-# general template of a function defining a control law
-# =============================================================================
-def my_control_law(t, robotNo, robots_poses):
-# =============================================================================  
-
-    # --- example of modification of global variables ---
-    # ---(updated values of global variables will be known at next call of this funtion) ---
-    # global toto
-    # toto = toto +1
-
-    # number of robots
-    nbOfRobots= robots_poses.shape[0]
-    
-    
-    # control law
-    vx = 0.
-    vy = 0.
-
-    # .................  TO BE COMPLETED HERE .............................
-    
-    return vx, vy
-# =============================================================================
-
+    return ui[0], ui[1]
